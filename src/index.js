98309afdb315f71980aeb52e8195c5196c73f8a5
ktscates <cates2506@gmail.com>
2024-09-10 10:55:20 +0200

    build tool showcase

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import "./styles.scss";
-import lodash from "lodash";
+import _ from "lodash";
 import toolsData from "./data/tools.json";
 
 function renderTools(tools) {
   const content = document.getElementById("content");
-  content.innerHTML = lodash
-    .map(
-      tools,
-      (tool) => `<p><strong>${tool.name}</strong>: ${tool.description}</p>`
-    )
-    .join("");
+  content.innerHTML = _.map(
+    tools,
+    (tool) => `<p><strong>${tool.name}</strong>: ${tool.description}</p>`
+  ).join("");
 }
 
 renderTools(toolsData);
